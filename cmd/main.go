56cb70169cae2bccf8d2270b5eb4e842fd28b5a8
stevec <stephenlclarke@mac.com>
2024-05-10 17:26:07 +0100

    Removed unused modules, Added some cmdline args

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jpxcz/sqlterm/databases"
 	mysqlclient "github.com/jpxcz/sqlterm/mysql_client"
 )
 
 func main() {
-	dbs := databases.GetDatabases()
-    fmt.Println("Welcome, please select one of the databases to connect")
-	for i, db := range dbs {
-		fmt.Printf("[%d] %v\n", i, db.ShortName)
+	// Get the database info
+	dbs, databaseMap, databaseKeys := databases.GetDatabases()
+
+	// Construct the command line args
+	var dbEnv string
+	flag.StringVar(&dbEnv, "env", dbs[0].Key, "One of the following database environment keys; ["+strings.Join(databaseKeys, ",")+"]")
+
+	var dbTableFormat string
+	flag.StringVar(&dbTableFormat, "table", "NO", "Format SQL tables [YES / NO]")
+	flag.Parse()
+
+	// Create a map of args that were supplied on the command line
+	flagset := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { flagset[f.Name] = true })
+
+	if flagset["env"] {
+		// If the environment was specified on the command line then use it
+		dbCreds := databaseMap[dbEnv]
+		mysqlclient.ExecMySqlClient(dbCreds.Username, dbCreds.Hostname, dbCreds.Password, flagset["table"])
+	} else {
+		// If the option wasn't supplioed on the command line then ask for it
+		fmt.Println("Welcome, please select one of the databases to connect")
+		for i, db := range dbs {
+			fmt.Printf("[%d] %s - %s\n", i, db.Key, db.ShortName)
+		}
+
+		var i int
+		fmt.Scanf("%d\n", &i)
+		if i >= len(dbs) {
+			log.Fatalf("Exiting: option [%d] selected is not in range of the databases. Exiting application\n", i)
+			os.Exit(1)
+		}
+
+		dbCreds := dbs[i]
+		mysqlclient.ExecMySqlClient(dbCreds.Username, dbCreds.Hostname, dbCreds.Password, flagset["table"])
 	}
 
-	var i int
-	fmt.Scanf("%d\n", &i)
-	if i >= len(dbs) {
-		log.Fatalf("option [%d] selected is not in range of the databases. Exiting application\n", i)
-		os.Exit(1)
-	}
-
-	db := dbs[i]
-	mysqlclient.ExecMySqlClient(db.Username, db.Hostname, db.Password)
 }
